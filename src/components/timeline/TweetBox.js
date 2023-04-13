e726a72515ad9898655223a66797056164246b54
focta <focke.wulf7234@gmail.com>
2023-04-13 18:14:28 +0900

    FireStoreへの登録を実施して、保存や再読み込み時に適用されることを確認

--- src/components/timeline/TweetBox.js
+++ src/components/timeline/TweetBox.js
@@ -1,21 +1,50 @@
 import { Avatar, Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import "./TweetBox.css";
+import { collection, addDoc } from "firebase/firestore";
+import db from "../../firebase";
 
 function TweetBox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    // firebaseのデータベースにデータを追加する。
+    // これはformでボタン押下時に画面更新されないように実装する。
+    e.preventDefault();
+    addDoc(collection(db, "posts"), {
+      displayName: "プログラミングチュートリアル",
+      userName: "sykt_tkhrm",
+      verified: true,
+      text: tweetMessage,
+      avator: "http://shincode.info/wp-content/uploads/2021/12/icon.png",
+      image: "https://source.unsplash.com/random",
+    });
+
+    setTweetMessage("");
+    setTweetImage("");
+  };
+
   return (
     <div className="tweetBox">
       <form>
         <div className="tweetBox_input">
           <Avatar />
-          <input placeholder="今どうしている？" type="text" />
+          <input
+            placeholder="今どうしている？"
+            type="text"
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+          />
         </div>
         <input
           className="tweetBox_imageInput"
           placeholder="画像のURLを入力してください"
           type="text"
+          onChange={(e) => setTweetImage(e.target.value)}
+          value={tweetImage}
         />
-        <Button className="tweet_tweetButton" type="submit">
+        <Button className="tweet_tweetButton" type="submit" onClick={sendTweet}>
           ツイートする
         </Button>
       </form>
