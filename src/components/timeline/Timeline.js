12c8da96f1cb666818c985f43844a2059e2063ef
focta <focke.wulf7234@gmail.com>
2023-04-14 22:54:52 +0900

    FireStoreのデータを更新したらすぐに新しいデータを取得できるようにした。

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -3,7 +3,13 @@ import "./Timeline.css";
 import TweetBox from "./TweetBox";
 import Post from "./Post";
 import db from "../../firebase";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 
 function Timeline() {
   const [posts, setPosts] = useState([]);
@@ -13,12 +19,19 @@ function Timeline() {
     // FireStoreの取得部分
     const postData = collection(db, "posts");
     const q = query(postData, orderBy("timestamp", "desc"));
-    getDocs(q).then((querySnapshot) => {
-      // 取得データを最新順に並べるための実装
-      setPosts(querySnapshot.docs.map((doc) => doc.data()));
-    });
+    // getDocsは呼ばれた時に一度だけデータを取得する
+    // useEffect内部で読んでいるために、初回レンダリング時の１回しか呼ばれず、Tweetの投稿後にデータが取得できない
+    // getDocs(q).then((querySnapshot) => {
+    // 取得データを最新順に並べるための実装
+    //   setPosts(querySnapshot.docs.map((doc) => doc.data()));
+    // });
     // 下記のコンソールは実際は strictModeを利用しているので２回出力してしまうが、レンダリングの度に呼び出される現象は防げている
     console.log("本当に１度きりしかよばれない・・・？");
+
+    // こちらは getDocsとは違い一度宣言することで、データ変更があった場合にデータ取得を行ってくれる。
+    onSnapshot(q, (querySnapshot) => {
+      setPosts(querySnapshot.docs.map((doc) => doc.data()));
+    });
   }, []);
 
   return (
@@ -29,7 +42,6 @@ function Timeline() {
       </div>
       {/* TweetBox */}
       <TweetBox />
-
       {posts.map((post) => (
         <Post
           key={post.text}
